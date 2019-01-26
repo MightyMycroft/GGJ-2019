85ae8bb65024fe036d5d4c6817e3ad5a9a2ca6e4
Rares-Sebastian Popa <Rares_Popa_2008@Yahoo.com>
2019-01-26 19:48:45 +0100

    Basic UI

---
+++ Assets/Scripts/SteeringWheel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheel : MonoBehaviour
+{
+    BoatController playerBoat;
+
+    public void Awake()
+    {
+        playerBoat = GameObject.FindWithTag("Player").GetComponent<BoatController>();
+    }
+
+    void Update()
+    {
+        transform.localRotation =Quaternion.Euler(0, 0, playerBoat.CurrentTorque * 300);
+    }
+}
