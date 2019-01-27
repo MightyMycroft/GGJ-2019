5c51971761f44263aa703b02092812a7d3a49215
Rares-Sebastian Popa <Rares_Popa_2008@Yahoo.com>
2019-01-27 14:26:01 +0100

    More tweaks to stuff

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -7,16 +7,19 @@ public class Speedometer : MonoBehaviour
     public float minAngle = -75;
     public float maxAngle;
 
+    BoatController boatController;
+
     Transform SpeedRod;
 
     private void Awake()
     {
+        boatController = GameObject.FindWithTag("Player").GetComponent<BoatController>();
+
         SpeedRod = transform.GetChild(0).GetChild(0);
     }
 
     void Update()
     {
-        var input = Input.GetKey(KeyCode.Space);
-        SpeedRod.localRotation = Quaternion.Slerp(SpeedRod.localRotation, Quaternion.Euler(input ? maxAngle : minAngle, 0, 0), Time.deltaTime * 1.5f);
+        SpeedRod.localRotation = Quaternion.Slerp(SpeedRod.localRotation, Quaternion.Euler(minAngle - boatController.NormalizedSpeed * minAngle, 0, 0), Time.deltaTime * 1.5f);
     }
 }
