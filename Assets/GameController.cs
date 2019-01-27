d15904bb7fb4e15fd50ba27cad86956ffbdffe68
NikoAmc <nl@amcentret.dk>
2019-01-27 13:16:24 +0100

    Added sailor and updated boat

---
+++ Assets/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Animator animator;
+    private static float time = 0f;
+    public float maxTimeInSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncreaseTime();
+    }
+
+    private static void IncreaseTime()
+    {
+        time += Time.deltaTime;
+    }
+
+    public static float GetTime()
+    {
+        return time;
+    }
+
+    private void CheckCrackenAnimation()
+    {
+        if(time > maxTimeInSeconds)
+        {
+            if(animator.GetBool("AttackCracken") == false)
+            {
+                animator.SetBool("AttackCracken", true);
+            }
+        }
+    }
+
+    public void ResetGame()
+    {
+        time = 0f;
+        animator.SetBool("AttackCracken", false);
+    }
+
+}
